fd140faed8177bf735dc6266ef030a4d32d546ea
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-05-31 15:57:21 +0200

    ordino lista

--- js/script.js
+++ js/script.js
@@ -21,3 +21,25 @@ for (i = 0; i < listaEmail.length; i++){
 document.getElementById('controllo').innerHTML = check;
 
 
+
+// Lista Cognomi:
+// chiedi all’utente il cognome
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+
+// creo una lista con dei cognomi 
+var listaCognomi = ['Neri','Verdi','Gialli','Bianchi','Rossi'];
+
+// chiedo all'utente un cognome 
+var cognome = prompt('dammi il tuo cognome');
+
+// inserisco il cognome nella lista 
+listaCognomi.push(cognome);
+
+//ordino alfabeticamente la lista
+listaCognomi = listaCognomi.sort();
+
+
+
