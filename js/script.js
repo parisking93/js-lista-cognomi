0d8ce6802208436b0fd8edfef0d09c56fa247d1c
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-05-31 17:05:45 +0200

    add while nel primo esercizio

--- js/script.js
+++ js/script.js
@@ -5,17 +5,24 @@
 
 // creo lista con email 
 var listaEmail = ['astrubale@icloud.com', 'gino@gmail.com', 'franc@gmail.com'];
-
+var check = 'hai sbagliato email riprova';
+var continua = true;
 // chiedo all'utente la sua email 
 var email = prompt('dammi la tua email');
 
-// controllo che l'email sia nella lista 
-var check = 'hai sbagliato email riprova';
-for (i = 0; i < listaEmail.length; i++){
-    if (listaEmail[i] == email) {
-        check = 'Bentornato ' + email;
-    } 
+while (continua == true) {
+    // controllo che l'email sia nella lista 
+    for (i = 0; i < listaEmail.length; i++){
+        if (listaEmail[i] == email) {
+            check = 'Bentornato ' + email;
+            continua = false;
+        } 
+    }
+
+    var email = prompt('dammi la tua email');
 }
+
+
 
 // stampo un messaggio appropriato  sull'esito del controllo 
 document.getElementById('controllo').innerHTML = check;
